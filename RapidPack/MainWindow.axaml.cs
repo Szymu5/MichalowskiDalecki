5f1e08415699c311b04bdcbb9296cfd954ec53c5
gutek28 <jakubgutowski@cyfroweszkoly.pl>
2026-03-19 21:01:21 +0100

    Text plus dodanie logiki

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
 
 namespace RapidPack;
 
@@ -7,5 +9,16 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        PrzyciskWyceny.Click += OnCalculateClick;
     }
-}
+
+    private void OnCalculateClick(object? sender, RoutedEventArgs e)
+    {
+        double.TryParse(WeightTextBox.Text, out double waga);
+        double.TryParse(HeightTextBox.Text, out double wys);
+        double.TryParse(WidthTextBox.Text, out double szer);
+        double.TryParse(DepthTextBox.Text, out double gleb);
+        
+        bool ekspres = PrzesylkaEkspresowa.IsChecked ?? false;
+        int typIndex = TypPrzesylki.SelectedIndex; 
+
