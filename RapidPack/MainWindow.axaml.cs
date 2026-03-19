116afe06c0c94ffde9d9d4bec32e881de1b9399d
gutek28 <jakubgutowski@cyfroweszkoly.pl>
2026-03-19 21:01:48 +0100

    Logika Koniec

--- RapidPack/MainWindow.axaml.cs
+++ RapidPack/MainWindow.axaml.cs
@@ -22,3 +22,26 @@ public partial class MainWindow : Window
         bool ekspres = PrzesylkaEkspresowa.IsChecked ?? false;
         int typIndex = TypPrzesylki.SelectedIndex; 
 
+        if (waga > 30) 
+        {
+            Podsumowanie.Text = "BŁĄD: Max 30kg!";
+            return;
+        }
+
+        double wynik = 0;
+
+        if (typIndex == 2) 
+        {
+            wynik = 100.0;
+        }
+        else 
+        {
+            wynik = 10.0 + (waga * 2.0);
+            if (typIndex == 1) wynik += 10.0;
+            if ((wys + szer + gleb) > 150) wynik *= 1.5;
+            if (ekspres) wynik += 15.0;
+        }
+
+        Podsumowanie.Text = $"CENA: {wynik:N2} zł";
+    }
+}
